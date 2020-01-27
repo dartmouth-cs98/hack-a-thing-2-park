c7f49661e2a120c0422b62d6b98c2ca5dceb29e0
soohwan0905 <soohwan0905@gmail.com>
2020-01-26 23:44:46 -0500

    Flying Platform + Updated README

---
+++ Flying Platform/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    //movement target
+    public Transform[] targets;
+
+    //speed
+    public float speed=10;
+
+    bool isMoving=false;
+
+    //next destination index
+    int nextIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position=targets[0].position;
+    
+        nextIndex=1;
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInput();
+        if (isMoving){
+            HandleMovement();
+        }
+    }
+
+    void HandleInput(){
+        if (Input.GetButtonDown("Fire1")){
+            isMoving=true;
+        }
+    }
+
+    void HandleMovement(){
+                //calculate distance from target
+        float distance = Vector3.Distance(transform.position, targets[nextIndex].position);
+        //have we arrived?
+            //calculate how much we need to move (step)
+        if (distance > 0) {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targets[nextIndex].position, step);
+        
+        }
+
+        //if we have arrived
+        else {
+            nextIndex++;
+            isMoving=false;
+            if (nextIndex==4){
+                nextIndex=0;
+            }
+        }
+            //move by that step
+    }
+}
